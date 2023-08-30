3226809c818e4f4593838b7f0bb71748cbf03bfc
Antonio Silva <antoniofranciscosilva.sp@gmail.com>
2023-08-29 21:57:28 -0300

    adicionar componentes

--- Livraria/FrontEnd/src/app/app.module.ts
+++ Livraria/FrontEnd/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { LivrariaAppComponent } from './components/livraria-app/livraria-app.component';
+import { FiltersComponent } from './components/livraria-app/filters/filters.component';
+import { ProductListComponent } from './components/livraria-app/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    LivrariaAppComponent,
+    FiltersComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
